0f1e204c30b2387f40ae3882a53c473558117393
Abhishek Bhosale <abhishekbhosale676@gmail.com>
2025-01-15 14:44:28 +0530

    done with extra practice paart2

---
+++ Challenge/Store.java
@@ -0,0 +1,28 @@
+package Challenge;
+
+public class Store {
+
+    private Movie[] movies;
+
+    public Store() {
+        this.movies = new Movie[10];
+    }
+
+    public Movie getMovie(int index) {
+        Movie copy=new Movie(this.movies[index]);
+        return copy;
+    }
+
+    public void setMovie(int index, Movie movie) {
+        this.movies[index] = new Movie(movie);
+    }
+
+    public String toString() {
+        String temp = "";
+        for (Movie movie : movies) {
+            temp += movie.toString() + "\n";
+        }
+        return temp;
+    }
+
+}
