525897a6d20cfe9b7e887254e4cb0a5c62d616aa
Abhishek Bhosale <abhishekbhosale676@gmail.com>
2025-01-19 14:01:41 +0530

    added higher order function with stream function

---
+++ HigherOrderFunc/Main.java
@@ -0,0 +1,25 @@
+package HigherOrderFunc;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+   public static void main(String[] args) {
+
+        List<Double> prices = Arrays.asList(341.67, 209.32, 88.41, 269.99, 68.49, 499.99, 28.12, 354.38);
+        prices.stream()
+            .filter(price -> {
+                return price > 100;
+            }).map(price -> {
+                return price-20;
+            }).sorted((right,left)->{
+                return right.compareTo(left);
+            }).map(price -> {
+                return "$"+price;
+            }).forEach(price ->{
+                System.out.println(price);
+            });
+    }
+}
